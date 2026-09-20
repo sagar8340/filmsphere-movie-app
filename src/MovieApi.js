49a65a62bf2a4da64c10111a0a6887fec3cc2c51
sagar8340 <sagartaidu007@gmail.com>
2026-09-20 13:30:38 +0530

    Update base url with env

--- src/MovieApi.js
+++ src/MovieApi.js
@@ -1,5 +1,7 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+
 export const fetch_movie_list = () => {
-    return fetch('http://localhost:5000/movie')
+    return fetch(`${BASE_URL}/movie`)
         .then((res) => res.json())
         .then((movie_Data) => {
             return movie_Data.results;
